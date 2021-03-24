21557e629e7565a77a34ba58aaa0b20f1ffe1e30
Mahabub Sunny <office.prosany@gmail.com>
2021-03-24 22:02:43 +0600

    POST and GET

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json())
 // without arrow function 
 // function callRoot(req, res) {
 //     res.send('Thank you for calling')
 // }
 
+// Get 
 app.get('/', (req, res) => {
     const products = {
         productName: 'Orange',
@@ -24,10 +30,18 @@ app.get('/user/:id', (req, res) => {
     // console.log(req.params.id);
     const id = req.params.id;
     console.log(req.query.sort);
-    const dynamic = users[id];
-    res.send({id, dynamic});
+    const userName = users[id];
+    res.send({id, userName});
 })
 
+
+// post
+app.post('/adduser', (req, res) => {
+    // console.log('Post Recived', req.body);
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
 
 
 app.listen(3000, () => console.log('listening to port 3000'));
